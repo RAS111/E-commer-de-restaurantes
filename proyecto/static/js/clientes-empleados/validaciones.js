1b1cfa970bae3d7886a5395a2f0ab9357a45c588
RAS111 <ras123gg@gmail.com>
2020-07-15 05:10:12 -0300

    se agrega la carpeta static y iconos

---
+++ proyecto/static/js/clientes-empleados/validaciones.js
@@ -0,0 +1,41 @@
+function validarDatos() {
+    var nombre = document.getElementById("txtNombre").value;
+    var apellido = document.getElementById("txtApellido").value;
+    var tipoDocumento = document.getElementById("cboTipoDocumento").value;
+    var numeroDocumento = document.getElementById("txtNumeroDocumento").value;
+    
+    // NOMBRE
+    if (nombre.trim() == "") {
+        alert("El nombre no debe estar vacio");
+        return;
+    } else if (nombre.length < 3 ) {
+        alert("El nombre debe contener al menos 3 caracteres");
+        return;
+    }
+
+    // APELLIDO
+    if (apellido.trim() == "" ) {
+        alert("El apellido no debe estar vacio");
+        return;
+    } else if (apellido.length < 3) {
+        alert("El apellido debe contener al menos 3 caracteres");
+        return;
+    }
+
+    // TIPO DOCUMENTO
+    if(tipoDocumento == 0){
+        alert("Debe seleccionar el documento")
+    }
+
+     // NUMERO DOCUMENTO
+    if (numeroDocumento.trim() == "") {
+        alert("El numero de documento no debe estar vacio");
+        return;
+    } else if(numeroDocumento.length < 8) {
+        alert("el numero de documento debe contener al menos 8 caracteres");
+        return;
+    }
+
+    var form = document.getElementById("frmDatos");
+    form.submit();
+}
---
+++ proyecto/static/js/productos/validaciones.js
@@ -0,0 +1,47 @@
+function validarDatos() {
+    var nombre = document.getElementById("txtNombre").value;
+    var precio = document.getElementById("txtPrecio").value;
+    var stockMinimo = document.getElementById("txtStockMinimo").value;
+    var stockActual = document.getElementById("txtStockActual").value;
+    var stockMaximo = document.getElementById("txtStockMaximo").value;
+    var rubro = document.getElementById("cboRubro").value;
+
+    // NOMBRE
+    if (nombre.trim() == "") {
+        alert("El nombre no debe estar vacio");
+        return;
+    } else if (nombre.length < 3 ) {
+        alert("El nombre debe contener al menos 3 caracteres");
+        return;
+    }
+
+    // PRECIO
+    if (precio.trim() == "" ) {
+        alert("el precio no debe estar vacio");
+        return;
+    } 
+
+     // STOCK
+    if (stockMinimo.trim() == "") {
+        alert("la cantidad de stock minima no debe estar vacia");
+        return;
+    } 
+
+    if (stockActual.trim() == "") {
+        alert("la cantidad de stock actual no debe estar vacia");
+        return;
+    } 
+
+    if (stockMaximo.trim() == "") {
+        alert("la cantidad de stock maximo no debe estar vacia");
+        return;
+    } 
+
+    // TIPO DOCUMENTO
+    if(rubro == 0){
+        alert("Debe seleccionar el rubro")
+    }
+
+    var form = document.getElementById("frmDatos");
+    form.submit();
+}
